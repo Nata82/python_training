dcc734522ccbb764907fe6b60c6e11422fbb61ac
nmetelkina <N.Metelkina@sirena-travel.ru>
2018-04-12 14:59:43 +0300

    конечное задание №3

---
+++ add_new.py
@@ -0,0 +1,31 @@
+
+
+
+class ADD_NEW:
+
+    def __init__(self,name, middlename, lastname, nickname, title, company, address, mobile, work,fax,
+                 email, byear, address2, phone2, notes):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.byear = byear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
+
+
+
+
+
+
+
+
