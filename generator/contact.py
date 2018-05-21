60b21fe16509e1873e602352e044c71edbd83154
nmetelkina <N.Metelkina@sirena-travel.ru>
2018-05-21 10:17:19 +0300

    дз 18

--- generator/contact.py
+++ generator/contact.py
@@ -53,6 +53,7 @@ testdata = [CONTACT(name="", middlename="", lastname="", nickname="",
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
+
 with open(file, "w") as out:
     jsonpickle.set_encoder_options("json", indent=2)
     out.write(jsonpickle.encode(testdata))
