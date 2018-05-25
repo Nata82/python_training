6652a91e453e5fdad25f2dc5f7ddc3abb26627ec
nmetelkina <N.Metelkina@sirena-travel.ru>
2018-05-25 13:00:43 +0300

    дз 21

--- test/test_contact_mail_address.py
+++ test/test_contact_mail_address.py
@@ -1,13 +1,10 @@
 import re
+from model.contact import CONTACT
 
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.add_new.get_add_new_list()[0]
-    contact_from_edit_page = app.add_new.get_add_new_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-
+def test_phones_on_home_page(app, db):
+    contact_from_home_page = app.add_new.get_add_new_list()
+    contact_from_homes_page = db.get_add_new_list()
+    assert sorted(contact_from_homes_page, key=CONTACT.id_or_max) == sorted(contact_from_home_page, key=CONTACT.id_or_max)
 
 
 def clear(s):
